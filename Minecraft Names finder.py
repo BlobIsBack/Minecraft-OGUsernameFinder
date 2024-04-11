f25648c54820dfd2fa61c524020b2deec4ecdbe3
BlobIsBack <145143384+BlobIsBack@users.noreply.github.com>
2024-04-11 17:59:49 +0200

    Update Minecraft Names finder.py

--- Minecraft Names finder.py
+++ Minecraft Names finder.py
@@ -5,9 +5,9 @@ import time
 print("recherche de pseudo disponible parmis 100 mots aleatoire (doublons possible lors du choix des mots)")
 var = 0
 list = []
-for i in range(100):
-	url = requests.get('https://random-word-form.herokuapp.com/random/noun/')
-	response = str(url.json()).replace('"', '').replace("'", "").replace('[',"").replace(']',"").replace('-',"").replace('-',"")
+for i in range(25):
+	url = requests.get('https://random-word-api.herokuapp.com/word?lang=en')
+	response = str(url.json()).replace('"', '').replace("'", "").replace('[',"").replace(']',"").replace('-',"").replace('-',"").replace('.',"")
 	apimojang = requests.get(f'https://api.mojang.com/users/profiles/minecraft/{response}')
 	if apimojang.status_code == 404:
 		print(response + " est un pseudo disponible")
@@ -18,11 +18,11 @@ for i in range(100):
 		time.sleep(10)
 	else:
 		print(response + " est un pseudo indisponible")
-	time.sleep(0.2)
+	time.sleep(0.5)
 print('')
-print("recherche finie, " + str(var) + " pseudos on été trouvés")
+print("recherche finie, " + str(var) + " pseudos on été trouvés")
 print("")
-print("pseudo disponible ou bientot disponible (sauf exeptions) : " + str(set(list))) # en cas de liste vide, set() est affiché
+print("pseudo disponible ou bientot disponible (sauf exeptions) : " + str(set(list)).replace('{', "").replace('}', "").replace("'", "")) # en cas de liste vide, set() est affiché
 
 
 # Random words lists
